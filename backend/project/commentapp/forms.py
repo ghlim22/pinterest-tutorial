8d16bf5effe1272170de480939b54583c9ecdf4d
lim <gylim@student.42seoul.kr>
2024-07-04 22:10:06 +0900

    commentapp create view

--- backend/project/commentapp/forms.py
+++ backend/project/commentapp/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
 
 from . import models
+
+
 class CommentCreationForm(ModelForm):
     class Meta:
         model = models.Comment
-        fields = ['content']
+        fields = ["content"]
--- backend/project/commentapp/views.py
+++ backend/project/commentapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-
+from django.urls import reverse
 from django.views.generic import CreateView
 
-from . import forms
-from . import models
-
+from . import forms, models
 
 # Create your views here.
 
+
 class CommentCreateView(CreateView):
     model = models.Comment
+    form_class = forms.CommentCreationForm
+    template_name = "commentapp/create.html"
+
+    def form_valid(self, form):
+        return super
+
+    def get_success_url(self):
+        return reverse("articleapp:detail", kwargs={"pk": self.object.article.pk})
