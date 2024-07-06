a5d4257ca55d571b0d0bd87cec3aff8e3bdcf54a
Gyuhyun Lim <hyunxx14@gmail.com>
2024-07-06 13:57:52 +0900

    lecture 38-1

--- backend/project/commentapp/models.py
+++ backend/project/commentapp/models.py
@@ -1,11 +1,12 @@
+from articleapp.models import Article
 from django.contrib.auth.models import User
 from django.db import models
 
-from articleapp.models import Article
 # Create your models here.
 
+
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name="article", null=True)
-    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comment', null=True)
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name="comment", null=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="comment", null=True)
     content = models.TextField(null=False)
     created_at = models.DateField(auto_now=True)
